ec58be4b1aeda4ef2269405f3492ab6a9367393f
Richard Quigley <richard@rqwebdev.com>
2022-08-15 20:34:36 +0100

    [ADD] Chat room and message models to admin

--- chatty_channels/chatty_channels_app/admin.py
+++ chatty_channels/chatty_channels_app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from chatty_channels_app.models import ChatRoom, Message
+
 # Register your models here.
+admin.site.register(ChatRoom)
+admin.site.register(Message)
