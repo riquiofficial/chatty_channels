c23bb13e11fddb3c16c6f0c85ecf50fb2ae2dfb9
Richard Quigley <richard@rqwebdev.com>
2022-08-15 20:34:55 +0100

    [ADD] String methods for chat room and messages

--- chatty_channels/chatty_channels_app/models.py
+++ chatty_channels/chatty_channels_app/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class ChatRoom(models.Model):
     name = models.CharField(max_length=200, blank=False, unique=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name[:30]
 
 
 class Message(models.Model):
@@ -12,3 +16,5 @@ class Message(models.Model):
     body = models.CharField(max_length=500, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.room} - {self.sender}"
