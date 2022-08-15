0be744e195c8470a5cb0875a2e04acf02ed42c33
Richard Quigley <richard@rqwebdev.com>
2022-08-15 21:23:11 +0100

    [ADD] Show saved messages in the chat and display WebSocket messages in similar format

--- chatty_channels/chatty_channels_app/consumers.py
+++ chatty_channels/chatty_channels_app/consumers.py
@@ -1,6 +1,10 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .models import ChatRoom, Message
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -25,23 +29,26 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event['message']
-        print("sending to sockets...")
+        new_message = event['message']
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(text_data=json.dumps(event))
 
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        print("got ", message)
+        # save to the db
+        user = User.objects.get(pk=text_data_json['user'])
+        room = ChatRoom.objects.get(pk=self.room)
+        new_message = Message.objects.create(sender=user,
+                                             room=room, body=message)
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.group,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': new_message.body,
+                'user': new_message.sender.username,
+                'timestamp': new_message.timestamp.__str__(),
             }
         )
